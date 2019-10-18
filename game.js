c722792d2259d46aadb34617eaa242de427c94ca
Lukasz <lukasz.d.zajac@gmail.com>
2019-10-18 18:39:11 +0200

    Update game.js
    added class draw and statistics

--- game.js
+++ game.js
@@ -38,7 +38,58 @@ class Wallet { // przechowywanie stanu pieniedzy
 // const wallet = new Wallet(200);
 
 class Statistics {
-    constructor() {
-
+    constructor() { // tworze konstruktor przechpwujący wszystkie wyniki
+        this.gameResults = [{
+            win: true,
+            bid: 2
+        }, {
+            win: false,
+            bid: -10
+        }];
     }
+
+    addGameStatistic(win, bid) { // metoda dodajaca lub odejmujaca wynik
+        let result = { // tworzymy obiekt, w ktorym bede umieszczał games result
+            win: win,
+            bid: bid
+            // mozna tez samo: win, bid
+        }
+        console.log(result);
+        this.gameResults.push(result);
+    }
+
+    showGameStatistic() { // metoda wyswietlająca wszystkie dane na stronie, przetwarza tablice gameResult
+        let games = this.gameResults.length; // pobieram dlugosc tablicy
+        let wins = this.gameResults.filter(result => result.win).length; // callback, funkcja filtrująca tablice
+        //gameResults. jesli jest true jest zwracane 1. Pozostałe wyniki dadzą ilosc przegranych
+        let lossess = this.gameResults.filter(result => !result.win).length;
+        return [games, wins, lossess];
+    }
+
+
 }
+
+
+// const stats = new Statistics();
+
+class Draw {
+    constructor() {
+        this.options = ['red', 'green', 'blue']; // przechowuje nazwy kolorów które moga byc wylosowane
+        let _result = ['red', 'green', 'blue']; // wynik naszego losowania
+        this.getDrawResult = () => _result;
+    }
+
+    drawResult() {
+        let colors = [];
+        for (let i = 0; i < this.options.length; i++) {
+            const index = Math.floor(Math.random() * this.options.length);
+            const color = this.options[index];
+            console.log(color);
+            colors.push(color);
+        }
+        return colors;
+    }
+
+}
+
+const draw = new Draw();
